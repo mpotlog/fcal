905ec173e226b0c94ca0921f77631ff72e347309
mpotlog <36661748+mpotlog@users.noreply.github.com>
2021-11-16 11:15:00 +0200

    Add files via upload

---
+++ fcal.cc
@@ -0,0 +1,70 @@
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4UIExecutive.hh"
+
+#include "DetectorConstruction.hh"
+#include "PhysicsList.hh"
+#include "ActionInitialization.hh"
+
+#include "G4PhysListFactory.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4VisExecutive.hh"
+
+
+int main(int argc, char** argv)
+{
+  G4cout << "Aplicatie pornita..."  << G4endl;
+
+  G4UIExecutive* ui = 0;
+  if ( argc == 1 ) {
+    ui = new G4UIExecutive(argc, argv);
+  }
+
+  // Run manager
+  G4RunManager * runManager = new G4RunManager;
+
+  // Mandatory initialization classes
+  runManager->SetUserInitialization(new DetectorConstruction);
+  runManager->SetUserInitialization(new PhysicsList);
+
+ // //G4PhysListFactory factory;
+  //G4VModularPhysicsList *physicsList = factory.GetReferencePhysList("FTFP_BERT");
+ // G4VModularPhysicsList *physicsList = factory.GetReferencePhysList("QGSP_BERT");
+ //  physicsList->SetDefaultCutValue(0.001*mm);
+ // runManager->SetUserInitialization(physicsList);
+
+  // Optional user action classes
+   runManager->SetUserInitialization(new ActionInitialization());
+
+  // Initialize G4 kernel
+  runManager->Initialize();
+
+  // Initialize visualization
+  G4VisManager* visManager = new G4VisExecutive;
+  visManager->Initialize();
+
+  // Get the pointer to the User Interface manager
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+  // Process macro or start UI session
+  if ( ! ui ) {
+    // batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else {
+    // interactive mode
+    UImanager->ApplyCommand("/control/execute vrml1.mac");
+    ui->SessionStart();
+    delete ui;
+  }
+
+  // Delete the managers
+  delete visManager;
+  delete runManager;
+
+  G4cout << "Aplicatie incheiata cu succes."  << G4endl;
+
+  return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,31 @@
+1. GEOMETRY:
+- 15 absorbers (without absorber in front of first sensor)
+- 15 sensors 
+- all 4 sectors of sensors
+- 
+
+
+
+
+
+
+
+
+
+
+
+impr1        33    
+impr2        48
+impr3        63
+impr4        78
+impr5        93
+impr6       108    
+impr7       123
+impr8       138
+impr9       153
+impr10      168
+impr11      183
+impr12      198
+impr13      213
+impr14      228
+impr15      243
